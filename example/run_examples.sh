9d2172b0039f0cd9270d38c9b0f9d9ff73d2fde4
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2018-09-27 14:16:37 +0200

    Update examples/tests to add SPR mode

---
+++ example/run_examples.sh
@@ -0,0 +1,15 @@
+dataset=simuls_998
+gene_tree=${dataset}/raxmlGeneTree.newick
+alignment=${dataset}/alignment.msa
+species_tree=${dataset}/speciesTree.newick
+threads=4
+prefix=output
+
+echo "Running NNI example"
+strategy=NNI
+../build/bin/JointTreeSearch -g ${gene_tree} -a ${alignment} -s ${species_tree} --strategy ${strategy} -t ${threads} -p ${prefix}_${strategy} --verbose
+
+echo "Running SPR example"
+strategy=SPR
+../build/bin/JointTreeSearch -g ${gene_tree} -a ${alignment} -s ${species_tree} --strategy ${strategy} -t ${threads} -p ${prefix}_${strategy} --verbose
+
