4e3e77f74c522bc8192b08b6feae5e235862ad91
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2019-03-08 22:38:00 +0100

    Rename JointSearchLight to geco, reset the code in the main

---
+++ src/geco/geco.cpp
@@ -0,0 +1,42 @@
+#include "Arguments.hpp"
+#include <ParallelContext.hpp>
+#include <IO/Logger.hpp>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+
+int internal_main(int argc, char** argv, void* comm)
+{
+  // the order is very important
+  ParallelContext::init(comm); 
+  Logger::init();
+  Arguments arguments(argc, argv);
+  Logger::initFileOutput(arguments.output);
+  
+  arguments.printCommand();
+  arguments.printSummary();
+  
+  Logger::timed << "End of geco execution" << endl;
+  ParallelContext::finalize();
+  return 0;
+}
+
+
+#ifdef JOINTSEARCH_BUILD_AS_LIB
+
+extern "C" int dll_main(int argc, char** argv, void* comm)
+{
+  return internal_main(argc, argv, comm);
+}
+
+#else
+
+int main(int argc, char** argv)
+{
+  return internal_main(argc, argv, 0);
+}
+
+#endif
+
