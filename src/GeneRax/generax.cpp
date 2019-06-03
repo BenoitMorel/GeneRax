cd103661eda041e1264fed6cefc620f396e387a9
BenoitMorel <benomorel@gmail.com>
2019-06-03 14:24:26 +0200

    Revert "Add more dated logs to understand where we lose time at the start"
    This reverts commit 767e8adaf71e557448eccf8489c75b68bbed1156.

--- src/GeneRax/generax.cpp
+++ src/GeneRax/generax.cpp
@@ -368,7 +368,6 @@ void search(const std::vector<FamiliesFileParser::FamilyInfo> &initialFamilies,
     GeneRaxArguments &arguments)
 
 {
-  Logger::timed << "Start SPR search" << std::endl;
   double totalLibpllLL = 0.0;
   double totalRecLL = 0.0;
   long sumElapsedRates = 0;
@@ -462,8 +461,6 @@ int generax_main(int argc, char** argv, void* comm)
   // the order is very important
   ParallelContext::init(comm); 
   Logger::init();
-  ParallelContext::barrier();
-  Logger::timed << "All cores started" << std::endl;
   GeneRaxArguments arguments(argc, argv);
   srand(arguments.seed);
   FileSystem::mkdir(arguments.output, true);
@@ -473,7 +470,7 @@ int generax_main(int argc, char** argv, void* comm)
   arguments.printSummary();
 
   std::vector<FamiliesFileParser::FamilyInfo> initialFamilies = FamiliesFileParser::parseFamiliesFile(arguments.families);
-  Logger::timed << "Number of gene families: " << initialFamilies.size() << std::endl;
+  Logger::info << "Number of gene families: " << initialFamilies.size() << std::endl;
   initFolders(arguments.output, initialFamilies);
 
   switch (arguments.strategy) {
