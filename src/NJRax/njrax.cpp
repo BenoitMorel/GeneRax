52af2da15c51d4e3b48df32eecb729fc22db3a6f
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2020-04-13 22:14:24 +0200

    Add an NJRax executable

---
+++ src/NJRax/njrax.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <iostream>
+
+#include <NJ/MiniNJ.hpp>
+#include <NJ/Cherry.hpp>
+#include <trees/PLLRootedTree.hpp>
+#include <IO/Families.hpp>
+#include <util/enums.hpp>
+
+int main(int argc, char** argv)
+{
+
+  if (argc != 5) {
+    std::cerr << "Error: syntax is ./njrax algorithm input_gene_trees mapping_file output_species_tree " << std::endl;
+    std::cerr << "Possible algorithms are [MiniNJ, Cherry, NJst]" << std::endl;
+    std::cerr << "The mapping file should have one of the two following syntaxes:" << std::endl;
+   std::cerr << std::endl;
+    std::cerr << "species1:gene1;gene2;gene3" << std::endl;
+    std::cerr << "species2:gene4;gene6" << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "or" << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "gene1 species1" << std::endl;
+    std::cerr << "gene2 species1" << std::endl;
+    std::cerr << "gene3 species2" << std::endl;
+    exit(1);
+  }
+  unsigned int a = 1;
+  std::string algoStr(argv[a++]);
+  std::string inputGeneTrees(argv[a++]);
+  std::string inputMappingFile(argv[a++]);
+  std::string outputSpeciesTree(argv[a++]);
+
+  // let's hack to use GeneRax core interface
+  Families families;
+  FamilyInfo info;
+  info.startingGeneTree = inputGeneTrees;
+  info.mappingFile = inputMappingFile;
+  families.push_back(info);
+  SpeciesTreeAlgorithm speciesTreeAlgorithm = Enums::strToSpeciesTree(algoStr);
+
+    
+  std::unique_ptr<PLLRootedTree> speciesTree = nullptr;
+  switch (speciesTreeAlgorithm) {
+  case SpeciesTreeAlgorithm::MiniNJ:
+    speciesTree = MiniNJ::runMiniNJ(families); 
+    break;
+  case SpeciesTreeAlgorithm::NJst:
+    speciesTree = MiniNJ::runNJst(families); 
+    break;
+  case SpeciesTreeAlgorithm::Cherry:
+    speciesTree = Cherry::geneTreeCherry(families); 
+    break;
+  default:
+    std::cerr << "Error: invalid strategy algorithm" << std::endl;
+    exit(2);
+  }
+  speciesTree->save(outputSpeciesTree);
+  return 0;
+}
+
