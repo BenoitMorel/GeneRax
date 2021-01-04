976bfa49c7450400b3dba42811076b42b7328e70
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2021-01-04 22:27:52 +0100

    Add forgotten file

---
+++ src/core/util/Constants.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+
+const unsigned int DEFAULT_SPECIES_SPR_RADIUS = 1;
+const unsigned int DEFAULT_SPECIES_SMALL_ROOT_RADIUS = 3; 
+const unsigned int DEFAULT_SPECIES_BIG_ROOT_RADIUS = 5; 
+
+
+
+
+
