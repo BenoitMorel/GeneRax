7daae73f4acfb3dd794ca2350a086e1dd4b89593
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2021-09-19 21:06:03 +0200

    Add missing files

---
+++ src/core/search/UNNISearch.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <trees/PLLUnrootedTree.hpp>
+
+
+/**
+ * Swap B and C
+ */
+struct UNNIMove {
+  UNNIMove (pll_unode_t *e,
+      bool l): edge(e), left(l) {}
+  void apply();
+  pll_unode_t *getA() const {return edge->next->back;}
+  pll_unode_t *getB() const {return edge->next->next->back;}
+  pll_unode_t *getC() const {return left? edge->back->next->back : 
+    edge->back->next->next->back;}
+  pll_unode_t *getD() const {return !left? edge->back->next->back : 
+    edge->back->next->next->back;}
+
+  pll_unode_t *edge;
+  bool left;
+};
+
+/**
+ *  Interface for the object evaluating
+ *  the score to be optimized for an unrooted
+ *  tree search.
+ */
+class USearchEvaluator {
+public:
+  virtual ~USearchEvaluator() {}
+  virtual double eval(PLLUnrootedTree &tree) = 0;
+  virtual double evalNNI(PLLUnrootedTree &tree, UNNIMove &move) = 0;
+};
+
+
+/**
+ *
+ */
+class UNNISearch {
+public:
+  UNNISearch(PLLUnrootedTree &tree,
+      USearchEvaluator &evaluator):
+    _tree(tree),
+    _evaluator(evaluator) {}
+  double runRound();
+  double search();
+private:
+  PLLUnrootedTree &_tree;
+  USearchEvaluator &_evaluator;
+};
